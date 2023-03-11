28b09d4103f696da0097964338f29682b8fc3841
flben233 <1400070301@qq.com>
2023-03-12 00:07:29 +0800

    更新至SpringBoot2.7.9，更新FastJson等依赖

--- src/main/java/org/shirakawatyu/handixikebackend/service/impl/LibraryServiceImpl.java
+++ src/main/java/org/shirakawatyu/handixikebackend/service/impl/LibraryServiceImpl.java
@@ -3,7 +3,6 @@ package org.shirakawatyu.handixikebackend.service.impl;
 import com.alibaba.fastjson2.JSONObject;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -18,36 +17,25 @@ import org.springframework.web.client.RestTemplate;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class LibraryServiceImpl implements LibraryService {
 
     @Override
-    public Result getLibrary(HttpSession session)  throws IOException{
+    public Result getLibrary(HttpSession session) {
         BasicCookieStore cookieStore = (BasicCookieStore)session.getAttribute("cookieStore");
-        Cookie Tgc = cookieStore.getCookies().get(2);
-        List<Cookie> cookiesStore = cookieStore.getCookies();
-        Map<String, String> map = new HashMap<>();
-        for (Cookie cookie : cookiesStore) {
-           map.put(cookie.getName(), cookie.getValue());
-        }
-
         RestTemplate restTemplate = (RestTemplate) session.getAttribute("template");
-
             try {
-                Connection.Response execute = Jsoup.connect("http://cas.swust.edu.cn/authserver/login?service=http://202.115.162.45:8080/reader/hwthau.php").followRedirects(false).cookies(map).execute();
-
-                List<String> locations = execute.headers("Location");
-                Connection.Response execute1 = Jsoup.connect(locations.get(0)).followRedirects(false).cookies(map).execute();
-                List<String> headers = execute1.headers("Set-Cookie");
-            String[] split = headers.get(0).split("=");
-            session.setAttribute("token",split);
-            map.put(split[0],split[1]);
-            Connection.Response execute2 = Jsoup.connect("http://202.115.162.45:8080/reader/book_lst.php").followRedirects(false).cookies(map).execute();
-                Requests.get("http://202.115.162.45:8080/reader/book_lst.php", "",restTemplate);
+                Requests.get("http://202.115.162.45:8080/opac/search_adv.php", "", restTemplate);
+                List<Cookie> cookiesStore = cookieStore.getCookies();
+                for (Cookie cookie : cookiesStore) {
+                    if (cookie.getName().equals("PHPSESSID")) {
+                        session.setAttribute("token", new String[]{"PHPSESSID", cookie.getValue()});
+                    }
+                }
+                Requests.get("http://202.115.162.45:8080/reader/login.php", "http://202.115.162.45:8080/opac/search_adv.php", restTemplate);
+                Requests.get("http://202.115.162.45:8080/reader/book_lst.php", "", restTemplate);
                 ResponseEntity<String> entity = Requests.get("http://202.115.162.45:8080/reader/book_lst.php", "", restTemplate);//请求两遍才能进去
 
 
@@ -79,18 +67,11 @@ public class LibraryServiceImpl implements LibraryService {
                 return Result.ok().data(JSONObject.toJSONString(books));
 
 
-            }catch (IOException e){
+            } catch (Exception e){
                 e.printStackTrace();
             }
 
             return Result.fail();
-
-
-
-
-
-
-
     }
 
     @Override
@@ -99,7 +80,6 @@ public class LibraryServiceImpl implements LibraryService {
         String requestBody = "{\"searchWords\":[{\"fieldList\":[{\"fieldCode\":\"\",\"fieldValue\":\""+bookName+"\"}]}],\"filters\":[],\"limiter\":[],\"sortField\":\"relevance\",\"sortType\":\"desc\",\"pageSize\":20,\"pageCount\":"+page+",\"locale\":\"\",\"first\":true}";
         String url = "http://202.115.162.45:8080/opac/ajax_search_adv.php";
         Document post = Jsoup.connect(url).header("Content-Type", "application/json").requestBody(requestBody).cookie(token[0], token[1]).post();
-
         return Result.ok().data(post.text());
     }
 
