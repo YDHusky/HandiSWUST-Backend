8c329332795f194e993bcaf824495197f9d0d819
ShirakawaTyu <1400070301@qq.com>
2023-11-22 17:25:01 +0800

    修复未登录时不返回信息的问题

--- src/main/java/org/shirakawatyu/handixikebackend/config/LoginInterceptor.java
+++ src/main/java/org/shirakawatyu/handixikebackend/config/LoginInterceptor.java
@@ -1,13 +1,18 @@
 package org.shirakawatyu.handixikebackend.config;
 
+import com.alibaba.fastjson2.JSON;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.shirakawatyu.handixikebackend.api.LoginApi;
+import org.shirakawatyu.handixikebackend.common.Result;
+import org.shirakawatyu.handixikebackend.common.ResultCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
 
 /**
  * @author ShirakawaTyu
@@ -15,9 +20,14 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         HttpSession session = request.getSession();
         RestTemplate template = (RestTemplate) session.getAttribute("template");
-        return session.getAttribute("status") != null && template != null;
+        if (session.getAttribute("status") != null && template != null) {
+            return true;
+        } else {
+            response.getWriter().write(JSON.toJSONString(Result.fail().code(ResultCode.LOGOUT)));
+            return false;
+        }
     }
 }
