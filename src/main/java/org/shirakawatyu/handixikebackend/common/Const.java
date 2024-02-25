fc24689b9eed1507380bad7a5d3c17385a34b1fd
ShirakawaTyu <1400070301@qq.com>
2024-02-25 18:16:52 +0800

    改日期

--- src/main/java/org/shirakawatyu/handixikebackend/common/Const.java
+++ src/main/java/org/shirakawatyu/handixikebackend/common/Const.java
@@ -3,8 +3,8 @@ package org.shirakawatyu.handixikebackend.common;
 import org.shirakawatyu.handixikebackend.utils.DateUtil;
 
 public class Const {
-    public static final String CURRENT_TERM = "2023-2024-1";
-    public static final long CURRENT_TERM_LONG = 202320241;    // 2023-2024-1
-    public static final long START_DATE = DateUtil.getDate("2023-8-28");
-    public static final long END_DATE = DateUtil.getDate("2024-1-15");
+    public static final String CURRENT_TERM = "2023-2024-2";
+    public static final long CURRENT_TERM_LONG = 202320242;    // 2023-2024-1
+    public static final long START_DATE = DateUtil.getDate("2024-2-26");
+    public static final long END_DATE = DateUtil.getDate("2024-7-14");
 }
