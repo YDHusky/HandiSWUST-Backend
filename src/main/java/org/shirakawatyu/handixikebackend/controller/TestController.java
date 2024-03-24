e82fc67be72106a553eb80d493566a353d6c2b2e
ShirakawaTyu <1400070301@qq.com>
2024-03-24 19:33:35 +0800

    修改docker-compose

---
+++ src/main/java/org/shirakawatyu/handixikebackend/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.shirakawatyu.handixikebackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ShirakawaTyu
+ */
+@RestController
+public class TestController {
+    @GetMapping("/api/test/ping")
+    public String test() {
+        return "Pong!a";
+    }
+}
