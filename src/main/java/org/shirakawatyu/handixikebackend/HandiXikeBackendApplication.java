b178faaa1b28859117a66de8386374a3249dedb2
ShirakawaTyu <1400070301@qq.com>
2024-06-25 21:30:38 +0800

    other: 删去了一些调试用的配置

--- src/main/java/org/shirakawatyu/handixikebackend/HandiXikeBackendApplication.java
+++ src/main/java/org/shirakawatyu/handixikebackend/HandiXikeBackendApplication.java
@@ -1,12 +1,8 @@
 package org.shirakawatyu.handixikebackend;
 
-import org.apache.hc.client5.http.cookie.BasicCookieStore;
-import org.shirakawatyu.handixikebackend.utils.Requests;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.logging.Logger;
 
 /**
  * @author ShirakawaTyu
@@ -16,10 +12,6 @@ import java.util.logging.Logger;
 public class HandiXikeBackendApplication {
 
     public static void main(String[] args) {
-        try {
-            String ip = Requests.getForString("https://ipinfo.io/ip", "", new BasicCookieStore());
-            Logger.getLogger("HandiXikeBackendApplication").info("当前服务器IP地址：" + ip);
-        } catch (Exception ignored) {}
         SpringApplication.run(HandiXikeBackendApplication.class, args);
     }
 
