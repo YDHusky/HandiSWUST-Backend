08e4e83136e5dc99592c3c3c683fd4c3b5531c82
ShirakawaTyu <1400070301@qq.com>
2024-09-18 21:46:19 +0800

    fix: 临时改动，修复登录无效的问题

--- src/main/java/org/shirakawatyu/handixikebackend/config/MvcConfig.java
+++ src/main/java/org/shirakawatyu/handixikebackend/config/MvcConfig.java
@@ -1,13 +1,10 @@
 package org.shirakawatyu.handixikebackend.config;
 
 import lombok.RequiredArgsConstructor;
-import org.shirakawatyu.handixikebackend.config.interfaces.MatchableHandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * @author ShirakawaTyu
@@ -15,15 +12,31 @@ import java.util.function.Consumer;
 @Configuration
 @RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
-    private final List<MatchableHandlerInterceptor> interceptors;
+//    private final List<MatchableHandlerInterceptor> interceptors;
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+    @Autowired
+    private TimeInterceptor timeInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor)
+                .excludePathPatterns(sessionInterceptor.excludes())
+                .addPathPatterns(sessionInterceptor.includes());
+        registry.addInterceptor(timeInterceptor)
+                .excludePathPatterns(timeInterceptor.excludes())
+                .addPathPatterns(timeInterceptor.includes());
+        registry.addInterceptor(loginInterceptor)
+                .excludePathPatterns(loginInterceptor.excludes())
+                .addPathPatterns(loginInterceptor.includes());
 
-        Consumer<MatchableHandlerInterceptor> register = (i) -> registry.addInterceptor(i)
-                .excludePathPatterns(i.excludes())
-                .addPathPatterns(i.includes());
-
-        interceptors.forEach(register);
+//        Consumer<MatchableHandlerInterceptor> register = (i) -> registry.addInterceptor(i)
+//                .excludePathPatterns(i.excludes())
+//                .addPathPatterns(i.includes());
+//
+//        interceptors.forEach(register);
     }
 }
